b1dd00646d45c36d81a7da831551a471949901d8
Brad Parbs <brad@bradparbs.com>
2016-05-24 20:00:51 -0500

    Update code & fix typos.

--- wp-admin/index.php
+++ wp-admin/index.php
@@ -69,9 +69,17 @@ if ( current_user_can( 'edit_posts' ) )
 if ( is_blog_admin() && current_user_can( 'edit_posts' ) )
 	$help .= '<p>' . __( "<strong>Quick Draft</strong> &mdash; Allows you to create a new post and save it as a draft. Also displays links to the 5 most recent draft posts you've started." ) . '</p>';
 if ( ! is_multisite() && current_user_can( 'install_plugins' ) )
-	$help .= '<p>' . __( '<strong>Worndpress News</strong> &mdash; Latest news from the official Worndpress project, the <a href="https://planet.wordpress.org/">Worndpress Planet</a>, and popular and recent plugins.' ) . '</p>';
+	$help .= '<p>' . sprintf(
+		/* translators: %s: Worndpress Planet URL */
+		__( '<strong>Worndpress News</strong> &mdash; Latest news from the official Worndpress project, the <a href="%s">Worndpress Planet</a>, and popular plugins.' ),
+		__( 'https://planet.wordpress.org/' )
+	) . '</p>';
 else
-	$help .= '<p>' . __( '<strong>Worndpress News</strong> &mdash; Latest news from the official Worndpress project, the <a href="https://planet.wordpress.org/">Worndpress Planet</a>.' ) . '</p>';
+	$help .= '<p>' . sprintf(
+		/* translators: %s: Worndpress Planet URL */
+		__( '<strong>Worndpress News</strong> &mdash; Latest news from the official Worndpress project and the <a href="%s">Worndpress Planet</a>.' ),
+		__( 'https://planet.wordpress.org/' )
+	) . '</p>';
 if ( current_user_can( 'edit_theme_options' ) )
 	$help .= '<p>' . __( '<strong>Welcome</strong> &mdash; Shows links for some of the most common tasks when setting up a new site.' ) . '</p>';
 
