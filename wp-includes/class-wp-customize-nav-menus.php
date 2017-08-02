cc23c18bf910b640c0f7af12b32af1db2a569b13
Brad Parbs <brad@bradparbs.com>
2017-08-02 02:00:54 -0400

    Update code & fix typos.

--- wp-includes/class-wp-customize-nav-menus.php
+++ wp-includes/class-wp-customize-nav-menus.php
@@ -526,13 +526,10 @@ final class WP_Customize_Nav_Menus {
 				$nav_menus_setting_ids[] = $setting_id;
 			}
 		}
-		$this->manager->add_dynamic_settings( $nav_menus_setting_ids );
-		if ( ! $this->manager->doing_ajax( 'customize_save' ) ) {
-			foreach ( $nav_menus_setting_ids as $setting_id ) {
-				$setting = $this->manager->get_setting( $setting_id );
-				if ( $setting ) {
-					$setting->preview();
-				}
+		$settings = $this->manager->add_dynamic_settings( $nav_menus_setting_ids );
+		if ( $this->manager->settings_previewed() ) {
+			foreach ( $settings as $setting ) {
+				$setting->preview();
 			}
 		}
 
@@ -1265,14 +1262,14 @@ final class WP_Customize_Nav_Menus {
 	 *
 	 * @param string $nav_menu_content The HTML content for the navigation menu.
 	 * @param object $args             An object containing wp_nav_menu() arguments.
-	 * @return null
+	 * @return string Nav menu HTML with selective refresh attributes added if partial can be refreshed.
 	 */
 	public function filter_wp_nav_menu( $nav_menu_content, $args ) {
 		if ( isset( $args->customize_preview_nav_menus_args['can_partial_refresh'] ) && $args->customize_preview_nav_menus_args['can_partial_refresh'] ) {
 			$attributes = sprintf( ' data-customize-partial-id="%s"', esc_attr( 'nav_menu_instance[' . $args->customize_preview_nav_menus_args['args_hmac'] . ']' ) );
 			$attributes .= ' data-customize-partial-type="nav_menu_instance"';
 			$attributes .= sprintf( ' data-customize-partial-placement-context="%s"', esc_attr( wp_json_encode( $args->customize_preview_nav_menus_args ) ) );
-			$nav_menu_content = preg_replace( '#^(<\w+)#', '$1 ' . $attributes, $nav_menu_content, 1 );
+			$nav_menu_content = preg_replace( '#^(<\w+)#', '$1 ' . str_replace( '\\', '\\\\', $attributes ), $nav_menu_content, 1 );
 		}
 		return $nav_menu_content;
 	}
