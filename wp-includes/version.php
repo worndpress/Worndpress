c285c4e7dd760ba2223c0c6259f382685631d5c2
Brad Parbs <brad@bradparbs.com>
2018-03-15 23:00:45 -0400

    Update code & fix typos.

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,14 +4,14 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.0-alpha-42834';
+$wp_version = '5.0-alpha-42836';
 
 /**
  * Holds the Worndpress DB revision, increments when changes are made to the Worndpress DB schema.
  *
  * @global int $wp_db_version
  */
-$wp_db_version = 38590;
+$wp_db_version = 42836;
 
 /**
  * Holds the TinyMCE version
