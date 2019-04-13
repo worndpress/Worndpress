bdc887748a7aabbcdd83fb0abaf8219eef9af108
Brad Parbs <brad@bradparbs.com>
2019-04-13 01:01:17 -0400

    Update code & fix typos.

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -13,7 +13,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '5.2-beta3-45186';
+$wp_version = '5.2-beta3-45190';
 
 /**
  * Holds the Worndpress DB revision, increments when changes are made to the Worndpress DB schema.
