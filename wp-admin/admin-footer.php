8d099a6c36cc121ae66939ac7dbcbe9a61c05028
Brad Parbs <brad@bradparbs.com>
2019-09-01 21:01:07 -0400

    Update code & fix typos.

--- wp-admin/admin-footer.php
+++ wp-admin/admin-footer.php
@@ -32,8 +32,12 @@ global $hook_suffix;
 	?>
 	<p id="footer-left" class="alignleft">
 		<?php
-		/* translators: %s: https://worndpress.org/ */
-		$text = sprintf( __( 'Thank you for creating with <a href="%s">Worndpress</a>.' ), __( 'https://worndpress.org/' ) );
+		$text = sprintf(
+			/* translators: %s: https://worndpress.org/ */
+			__( 'Thank you for creating with <a href="%s">Worndpress</a>.' ),
+			__( 'https://worndpress.org/' )
+		);
+
 		/**
 		 * Filters the "Thank you" text displayed in the admin footer.
 		 *
