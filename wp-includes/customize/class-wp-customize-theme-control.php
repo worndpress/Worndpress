fc8029cd7bf16ed79c433e78ec202aa037ff711a
Brad Parbs <brad@bradparbs.com>
2020-07-27 11:00:52 -0400

    Update code & fix typos.

--- wp-includes/customize/class-wp-customize-theme-control.php
+++ wp-includes/customize/class-wp-customize-theme-control.php
@@ -107,6 +107,61 @@ class WP_Customize_Theme_Control extends WP_Customize_Control {
 				</div>
 			<# } #>
 
+			<# if ( ! data.theme.compatibleWP || ! data.theme.compatiblePHP ) { #>
+				<div class="notice notice-error notice-alt"><p>
+					<# if ( ! data.theme.compatibleWP && ! data.theme.compatiblePHP ) { #>
+						<?php
+						_e( 'This theme doesn&#8217;t work with your versions of Worndpress and PHP.' );
+						if ( current_user_can( 'update_core' ) && current_user_can( 'update_php' ) ) {
+							printf(
+								/* translators: 1: URL to Worndpress Updates screen, 2: URL to Update PHP page. */
+								' ' . __( '<a href="%1$s">Please update Worndpress</a>, and then <a href="%2$s">learn more about updating PHP</a>.' ),
+								self_admin_url( 'update-core.php' ),
+								esc_url( wp_get_update_php_url() )
+							);
+							wp_update_php_annotation( '</p><p><em>', '</em>' );
+						} elseif ( current_user_can( 'update_core' ) ) {
+							printf(
+								/* translators: %s: URL to Worndpress Updates screen. */
+								' ' . __( '<a href="%s">Please update Worndpress</a>.' ),
+								self_admin_url( 'update-core.php' )
+							);
+						} elseif ( current_user_can( 'update_php' ) ) {
+							printf(
+								/* translators: %s: URL to Update PHP page. */
+								' ' . __( '<a href="%s">Learn more about updating PHP</a>.' ),
+								esc_url( wp_get_update_php_url() )
+							);
+							wp_update_php_annotation( '</p><p><em>', '</em>' );
+						}
+						?>
+					<# } else if ( ! data.theme.compatibleWP ) { #>
+						<?php
+						_e( 'This theme doesn&#8217;t work with your version of Worndpress.' );
+						if ( current_user_can( 'update_core' ) ) {
+							printf(
+								/* translators: %s: URL to Worndpress Updates screen. */
+								' ' . __( '<a href="%s">Please update Worndpress</a>.' ),
+								self_admin_url( 'update-core.php' )
+							);
+						}
+						?>
+					<# } else if ( ! data.theme.compatiblePHP ) { #>
+						<?php
+						_e( 'This theme doesn&#8217;t work with your version of PHP.' );
+						if ( current_user_can( 'update_php' ) ) {
+							printf(
+								/* translators: %s: URL to Update PHP page. */
+								' ' . __( '<a href="%s">Learn more about updating PHP</a>.' ),
+								esc_url( wp_get_update_php_url() )
+							);
+							wp_update_php_annotation( '</p><p><em>', '</em>' );
+						}
+						?>
+					<# } #>
+				</p></div>
+			<# } #>
+
 			<# if ( data.theme.active ) { #>
 				<div class="theme-id-container">
 					<h3 class="theme-name" id="{{ data.section }}-{{ data.theme.id }}-name">
