2ce3e87eda1f5592678fdcf85731d77891f7c17f
Brad Parbs <brad@bradparbs.com>
2021-09-23 01:11:28 -0500

    Update code & fix typos.

--- wp-includes/widgets.php
+++ wp-includes/widgets.php
@@ -1255,15 +1255,17 @@ function _wp_sidebars_changed() {
 }
 
 /**
- * Validates and remaps any "orphaned" widgets to wp_inactive_sidgets sidebar, and saves the widget settings.
- * This has to run at least on each theme change.
+ * Validates and remaps any "orphaned" widgets to wp_inactive_widgets sidebar,
+ * and saves the widget settings. This has to run at least on each theme change.
  *
  * For example, let's say theme A has a "footer" sidebar, and theme B doesn't have one.
- * After switching from theme A to theme B, all the widgets previously assigned to the footer would be inaccessible.
- * This function detects this scenario, and moves all the widgets previously assigned to the footer under wp_inactive_widgets.
+ * After switching from theme A to theme B, all the widgets previously assigned
+ * to the footer would be inaccessible. This function detects this scenario, and
+ * moves all the widgets previously assigned to the footer under wp_inactive_widgets.
  *
- * Despite the word "retrieve" in the name, this function actually updates the database and the global $sidebars_widgets.
- * For that reason it should not be run from the front end unless the param value is 'customize' (to bypass the database write).
+ * Despite the word "retrieve" in the name, this function actually updates the database
+ * and the global `$sidebars_widgets`. For that reason it should not be run on front end,
+ * unless the `$theme_changed` value is 'customize' (to bypass the database write).
  *
  * @since 2.8.0
  *
@@ -1737,11 +1739,12 @@ function wp_widget_rss_process( $widget_rss, $check_feed = true ) {
 	$show_summary = isset( $widget_rss['show_summary'] ) ? (int) $widget_rss['show_summary'] : 0;
 	$show_author  = isset( $widget_rss['show_author'] ) ? (int) $widget_rss['show_author'] : 0;
 	$show_date    = isset( $widget_rss['show_date'] ) ? (int) $widget_rss['show_date'] : 0;
+	$error        = false;
+	$link         = '';
 
 	if ( $check_feed ) {
-		$rss   = fetch_feed( $url );
-		$error = false;
-		$link  = '';
+		$rss = fetch_feed( $url );
+
 		if ( is_wp_error( $rss ) ) {
 			$error = $rss->get_error_message();
 		} else {
